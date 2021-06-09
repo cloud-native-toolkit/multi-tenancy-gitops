cb46c55ceff42183abb3ee14fc273f69c532e878
hollisc <hollisc@ca.ibm.com>
2021-06-08 23:10:20 -0400

    Initial commit for MQ workstream based on latest from master

--- scripts/enable-apps.sh
+++ scripts/enable-apps.sh
@@ -6,6 +6,6 @@ SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOTDIR="$(cd ${SCRIPTDIR}/..; pwd )"
 [[ -n "${DEBUG:-}" ]] && set -x
 
-cp ${ROOTDIR}/0-bootstrap/argocd/inactive/1-apps.yaml ${ROOTDIR}/0-bootstrap/argocd/active
+cp ${ROOTDIR}/0-bootstrap/argocd/inactive/3-apps.yaml ${ROOTDIR}/0-bootstrap/argocd/active
 GIT_MESSAGE="enable apps"
 source ${SCRIPTDIR}/git-add-commit-push.sh
--- scripts/enable-infra.sh
+++ scripts/enable-infra.sh
@@ -6,6 +6,6 @@ SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOTDIR="$(cd ${SCRIPTDIR}/..; pwd )"
 [[ -n "${DEBUG:-}" ]] && set -x
 
-cp ${ROOTDIR}/0-bootstrap/argocd/inactive/3-infra.yaml ${ROOTDIR}/0-bootstrap/argocd/active
+cp ${ROOTDIR}/0-bootstrap/argocd/inactive/1-infra.yaml ${ROOTDIR}/0-bootstrap/argocd/active
 GIT_MESSAGE="enable infra"
 source ${SCRIPTDIR}/git-add-commit-push.sh
