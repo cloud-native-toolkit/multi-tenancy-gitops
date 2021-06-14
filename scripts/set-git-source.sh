9be387e7dbecff5a43e21652f67306bfed708744
hollisc <hollisc@ca.ibm.com>
2021-06-14 14:23:35 -0400

    Update script to include sed call for modifying cluster domain for certificate resources

--- scripts/set-git-source.sh
+++ scripts/set-git-source.sh
@@ -7,6 +7,8 @@ SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 GIT_USER=${GIT_USER}
 if [ -z ${GIT_USER} ]; then echo "Please set GIT_USER when running script, optional GIT_BASEURL and GIT_REPO to formed the git url GIT_BASEURL/GIT_USER/GIT_REPO"; exit 1; fi
+CLUSTER_DOMAIN=${CLUSTER_DOMAIN}
+if [ -z ${CLUSTER_DOMAIN} ]; then echo "CLUSTER_DOMAIN has not been set for the Certificate resources."; fi
 set -u
 
 GIT_REPO=${GIT_REPO:-multi-tenancy-gitops.git}
@@ -17,8 +19,8 @@ GIT_BRANCH=${GIT_BRANCH:-master}
 
 echo "Setting source git to ${GIT_BASEURL}/${GIT_USER}/${GIT_REPO}"
 
-find ${SCRIPTDIR}/.. -name '*.yaml' -print0 | 
-  while IFS= read -r -d '' File; do 
+find ${SCRIPTDIR}/.. -name '*.yaml' -print0 |
+  while IFS= read -r -d '' File; do
     if grep -q "kind: Application\|kind: AppProject" "$File"; then
       echo "$File"
       sed -i'.bak' -e "s#repoURL: https://github.com/cloud-native-toolkit/multi-tenancy-gitops.git#repoURL: ${GIT_BASEURL}/${GIT_USER}/${GIT_REPO}#" $File
@@ -27,6 +29,12 @@ find ${SCRIPTDIR}/.. -name '*.yaml' -print0 |
       sed -i'.bak' -e "s#targetRevision: master#targetRevision: ${GIT_BRANCH}#" $File
       rm "${File}.bak"
     fi
+    if grep -q "kind: Certificate" "$File"; then
+      echo "$File"
+      sed -i'.bak' -e "s/<cluster-domain>/$CLUSTER_DOMAIN/g" $File
+      rm "${File}.bak"
+    fi
   done
 
 echo "git commit and push changes now"
+
