4b87a6293ad45f307900e289493088c41ab592fb
Carlos Santana <csantana@us.ibm.com>
2021-08-24 23:57:20 -0400

    add cloudpak console url  output (#76)
    * add cloudpak console url  output
    
    Signed-off-by: Carlos Santana <csantana23@gmail.com>
    
    * remove extra echo
    
    Signed-off-by: Carlos Santana <csantana23@gmail.com>
    
    * make it easy to copy and paste
    
    Signed-off-by: Carlos Santana <csantana23@gmail.com>
    Signed-off-by: Tyler-Stanczak <tyler.stanczak@ibm.com>

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -361,15 +361,21 @@ ace_apps_bootstrap () {
 }
 
 print_urls_passwords () {
-    echo "Openshift Console UI: $(oc whoami --show-console)"
-    echo " "
-    echo "Openshift GitOps UI: $(oc get route -n openshift-gitops openshift-gitops-cntk-server -o template --template='https://{{.spec.host}}')"
-    echo " "
-    echo "To get the ArgoCD URL and admin password:"
-    echo "-----"
+
+    echo "# Openshift Console UI: $(oc whoami --show-console)"
+    echo "# "
+    echo "# Openshift ArgoCD/GitOps UI: $(oc get route -n openshift-gitops openshift-gitops-cntk-server -o template --template='https://{{.spec.host}}')"
+    echo "# "
+    echo "# To get the ArgoCD/GitOps URL and admin password:"
+    echo "# -----"
     echo "oc get route -n openshift-gitops openshift-gitops-cntk-server -o template --template='https://{{.spec.host}}'"
     echo "oc extract secrets/openshift-gitops-cntk-cluster --keys=admin.password -n openshift-gitops --to=-"
-    echo "-----"
+    echo "# -----"
+    echo "# The Cloud Pak console and admin password"
+    echo "oc get route -n tools integration-navigator-pn -o template --template='https://{{.spec.host}}'"
+    echo "oc extract -n ibm-common-services secrets/platform-auth-idp-credentials --keys=admin_username,admin_password --to=-"
+    echo "# -----"
+
 }
 
 # main
