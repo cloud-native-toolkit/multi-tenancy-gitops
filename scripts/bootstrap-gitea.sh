ef9df7bd90200cc8ddd474013586eae7a2cbc6ab
Larry Steck <lsteck@us.ibm.com>
2022-01-10 12:53:31 -0600

    combine Gitea extract commands
    Signed-off-by: Larry Steck <lsteck@us.ibm.com>

--- scripts/bootstrap-gitea.sh
+++ scripts/bootstrap-gitea.sh
@@ -410,12 +410,7 @@ print_urls_passwords () {
     echo "# "
     echo "# To get the Gitea admin ID and admin password:"
     echo "# -----"
-<<<<<<< HEAD
     echo "oc extract secrets/${INSTANCE_NAME}-access --keys=username,password -n ${TOOLKIT_NAMESPACE} --to=-"
-=======
-    echo "oc extract secrets/${INSTANCE_NAME}-access --keys=username -n ${TOOLKIT_NAMESPACE} --to=-"
-    echo "oc extract secrets/${INSTANCE_NAME}-access --keys=password -n ${TOOLKIT_NAMESPACE} --to=-"
->>>>>>> e3246cf (add bootstrap for Gitea)
     echo "# -----"
 
 }
